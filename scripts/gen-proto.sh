fa68e4f8900dbe28acce7412779f001eb72182e8
Mubina <mubinayigitaliyeva00@gmail.com>
2024-02-09 15:01:41 +0500

    finished

--- scripts/gen-proto.sh
+++ scripts/gen-proto.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
 CURRENT_DIR=$(pwd)
 
-protoc -I /usr/local/include \
-       -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
-       -I "$CURRENT_DIR/protos/" \
-        --gofast_out=plugins=grpc:"$CURRENT_DIR/genproto/" \
-        "$CURRENT_DIR/protos/post_service/post.proto";
+for module in $(find $CURRENT_DIR/protos/* -type d); do
+    protoc -I /usr/local/include \
+           -I $GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.2 \
+           -I $CURRENT_DIR/protos/ \
+            --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/ \
+            $module/*.proto;
+done;
+
+for module in $(find $CURRENT_DIR/genproto/* -type d); do
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i "" -e "s/,omitempty//g" $module/*.go
+  else
+    sed -i -e "s/,omitempty//g" $module/*.go
+  fi
+done;
+
